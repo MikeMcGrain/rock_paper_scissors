516fc6dea0249b97114d3ae45fca06337f75502a
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-23 14:57:35 -0600

    get and render functions

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
 window.addEventListener("load", function() {
-  document.getElementById("bear-btn").addEventListener("click", getUserPick)
-  document.getElementById("ninja-btn").addEventListener("click", getUserPick)
-  document.getElementById("cowboy-btn").addEventListener("click", getUserPick)
+  document.getElementById("bear-btn").addEventListener("click", startGame)
+  document.getElementById("ninja-btn").addEventListener("click", startGame)
+  document.getElementById("cowboy-btn").addEventListener("click", startGame)
 })
 
-function getUserPick() {
-  let userPick = this.getAttribute("data-user-pick")
+function startGame() {
+  let userPick = getUserPick(this)
+  let browserPick = getBrowserPick()
+  getResult(userPick, browserPick)
+}
+
+function getUserPick(e) {
+  let userPick = e.getAttribute("data-user-pick")
   document.getElementById("user-pick").innerHTML = `You picked ${userPick}`
-  renderImage(userPick, "user-pick")
-  comparePicks(userPick, getBrowserPick())
+  renderPick(userPick, "user-pick")
+  return userPick
 }
 
 function getBrowserPick() {
   let browserPick
   switch (Math.floor(Math.random() * 3)) {
-    case 0: browserPick = "Bear"; break
-    case 1: browserPick = "Ninja"; break
-    case 2: browserPick = "Cowboy"
+    case 0:
+      browserPick = "Bear"
+      break
+    case 1:
+      browserPick = "Ninja"
+      break
+    case 2:
+      browserPick = "Cowboy"
   }
-  document.getElementById("browser-pick").innerHTML = `Browser picked ${browserPick}`
-  renderImage(browserPick, "browser-pick")
+  document.getElementById(
+    "browser-pick"
+  ).innerHTML = `Browser picked ${browserPick}`
+  renderPick(browserPick, "browser-pick")
   return browserPick
 }
 
-function comparePicks(userPick, browserPick) {
-  //determine result: win, lose, tie
+function getResult(userPick, browserPick) {
   let result
-  if (userPick == browserPick) {result = "tie"} 
-  else {
+  if (userPick == browserPick) {
+    result = "tie"
+  } else {
     switch (userPick) {
       case "Bear":
         result = browserPick == "Ninja" ? "win" : "lose"
@@ -38,9 +51,31 @@ function comparePicks(userPick, browserPick) {
       case "Cowboy":
         result = browserPick == "Bear" ? "win" : "lose"
     }
-  }  
+  }
+  renderResult(result)
+}
 
-  //Style results
+function renderPick(pick, elementID) {
+  let location
+  switch (pick) {
+    case "Bear":
+      location = "images/claws.png"
+      break
+    case "Ninja":
+      location = "images/kapow.png"
+      break
+    case "Cowboy":
+      location = "images/bang.png"
+  }
+
+  let image = document.createElement("img")
+  image.setAttribute("src", location)
+  image.setAttribute("alt", pick)
+  let p = document.getElementById(elementID)
+  p.appendChild(image)
+}
+
+function renderResult(result) {
   let resultStyle
   switch (result) {
     case "tie":
@@ -53,24 +88,13 @@ function comparePicks(userPick, browserPick) {
       resultStyle = "font-size: 20vw; color: red"
   }
 
-  document.getElementById("result").innerHTML = `<span id="result-span" style="${resultStyle}"> ${result}</span>`
+  document.getElementById(
+    "result"
+  ).innerHTML = `<span id="result-span" style="${resultStyle}"> ${result}</span>`
   document.getElementById("overlay").style.display = "block"
-  document.getElementById("overlay").addEventListener("click", function(){
+  document.getElementById("overlay").addEventListener("click", function() {
     document.getElementById("overlay").style.display = "none"
+    document.getElementById("user-pick").innerHTML = ""
+    document.getElementById("browser-pick").innerHTML = ""
   })
-}
-
-function renderImage(pick, elementID) {
-  let location
-  switch (pick) {
-    case "Bear": location = "images/claws.png"; break
-    case "Ninja": location = "images/kapow.png"; break
-    case "Cowboy": location = "images/bang.png"
-  }
-
-  let image = document.createElement("img")
-  image.setAttribute("src", location)
-  image.setAttribute("alt", pick)
-  let p = document.getElementById(elementID)
-  p.appendChild(image)
 }
